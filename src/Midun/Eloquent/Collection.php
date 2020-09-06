1e230060fcb62fa134c4644f17b4b2ec4cf4665f
Vo Dang Anh <danganh.dev@gmail.com>
2020-09-06 19:29:31 +0700

    feature/beautify

--- src/Midun/Eloquent/Collection.php
+++ src/Midun/Eloquent/Collection.php
@@ -4,11 +4,24 @@ namespace Midun\Eloquent;
 
 class Collection extends \ArrayObject
 {
+    /**
+     * Initial collection
+     * 
+     * @param array $input = []
+     * @param int $flag
+     * 
+     * @return void
+     */
     public function __construct(array $input = [], int $flag = \ArrayObject::STD_PROP_LIST)
     {
         parent::__construct($input, $flag);
     }
 
+    /**
+     * Collection to array
+     * 
+     * @return array
+     */
     public function toArray()
     {
         $array = objectToArray($this);
